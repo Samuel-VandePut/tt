cb3f86bb17d37bc36bb5bcd6e78fa783d2633490
saM21785 <sam.vdp@hotmail.com>
2017-09-23 11:59:32 +0200

    modification du fichier config et database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/menuiserie_bodart_ajax/';
+$config['base_url'] = 'http://localhost/tt/';
 
 /*
 |--------------------------------------------------------------------------
